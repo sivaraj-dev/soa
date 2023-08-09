5cfd608612f0d78c4624b6ee2cc40c13445a708c
Sivaraj S <contact@sdev.in>
2023-08-09 12:33:25 +0530

    Hardcoded students list changed to array of objects

--- r03/src/Students.js
+++ r03/src/Students.js
@@ -1,4 +1,37 @@
 const Students = () => {
+  const students = [
+    {
+      id: 1,
+      regNo: "SOAMCA01",
+      name: "Student 1",
+      branch: "MCA",
+    },
+    {
+      id: 2,
+      regNo: "SOAMCA02",
+      name: "Student 2",
+      branch: "MCA",
+    },
+    {
+      id: 3,
+      regNo: "SOAMCA03",
+      name: "Student 3",
+      branch: "MCA",
+    },
+    {
+      id: 4,
+      regNo: "SOAMCA04",
+      name: "Student 4",
+      branch: "MCA",
+    },
+    {
+      id: 5,
+      regNo: "SOAMCA05",
+      name: "Student 5",
+      branch: "MCA",
+    },
+  ];
+
   return (
     <>
       <table border="1">
@@ -11,18 +44,16 @@ const Students = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>SOAMCA01</td>
-            <td>Student 1</td>
-            <td>MCA</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>SOAMCA02</td>
-            <td>Student 2</td>
-            <td>MCA</td>
-          </tr>
+          {students.map((student, index) => {
+            return (
+              <tr key={index}>
+                <td>{student.id}</td>
+                <td>{student.regNo}</td>
+                <td>{student.name}</td>
+                <td>{student.branch}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </>
