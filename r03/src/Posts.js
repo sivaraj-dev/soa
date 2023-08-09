f1ab81492c5555f9adc9cb3933c49c91021fa28d
Sivaraj S <contact@sdev.in>
2023-08-09 11:39:17 +0530

    Added some more properties to the post and adjusted the UI.

--- r03/src/Posts.js
+++ r03/src/Posts.js
@@ -1,19 +1,30 @@
 const Posts = () => {
   const posts = [
-    "Post 1",
-    "Post 2",
-    "Post 3",
-    "Post 4",
-    "Post 5",
-    "Post 6",
-    "Post 7",
+    {
+      id: 1,
+      title: "Post 1",
+      content: "This is my first post",
+      noLikes: 10,
+      noComments: 2,
+    },
   ];
 
   return (
     <>
       <ol>
         {posts.map((post, index) => {
-          return <li key={index}>{post}</li>;
+          return (
+            <li key={index}>
+              <p>
+                <strong>{post.title}</strong>
+              </p>
+              <p>{post.content}</p>
+              <p>
+                <strong>{post.noLikes}</strong> likes •{" "}
+                <strong>{post.noComments}</strong> Comments
+              </p>
+            </li>
+          );
         })}
       </ol>
     </>
