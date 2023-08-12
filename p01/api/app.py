8610bc7080f1f2a2b8ba816068326cf80a6670f5
Sivaraj S <contact@sdev.in>
2023-08-12 11:54:49 +0530

    added new routes for posts and return array of posts

---
+++ p01/api/app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return {
+        "status": 0,
+        "cls": "error",
+        "msg": "Access Denied",
+        "payload": {},
+    }
+
+
+@app.route("/posts")
+def posts():
+    posts = [
+        {
+            "id": 1,
+            "title": "Hello World",
+            "content": "This is my first post",
+        }
+    ]
+
+    return {
+        "status": 1,
+        "cls": "success",
+        "msg": "Success",
+        "payload": posts,
+    }
