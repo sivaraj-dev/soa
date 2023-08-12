ab7648a87d652cc9ad337034ba2df2643943b3ef
Sivaraj S <contact@sdev.in>
2023-08-12 10:20:11 +0530

    Added routes for the pages

--- r03/src/App.js
+++ r03/src/App.js
@@ -1,9 +1,37 @@
 import Login from "./Login";
+import Home from "./Home";
+import About from "./About";
+
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+  ]);
+
   return (
     <>
-      <Login />
+      <nav>
+        <a href="/">Home</a> <a href="/about">About</a>{" "}
+        <a href="/login">Login</a>
+        {/*
+        <Link to="/login">Login</Link>
+        <Link to="/about">About</Link> */}
+      </nav>
+      <main>
+        <RouterProvider router={router} />
+      </main>
     </>
   );
 };
