0b29238d812f2ef42abfc770e59bd9a7a3ee57a6
Sivaraj S <contact@sdev.in>
2023-08-14 12:07:44 +0530

    user dummy auth api

--- p03/api/app.py
+++ p03/api/app.py
@@ -1,22 +1,59 @@
-from flask import Flask
+from flask import Flask, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient("mongodb://localhost:27017/")
+mdb = client.m03
 
 
 @app.route("/")
 def home():
+    users = []
+    for item in mdb.users.find({}):
+        users.append(item)
+
     return {
         "status": 1,
         "cls": "success",
         "msg": "Home",
-        "payload": {},
+        "payload": {
+            "users": users,
+        },
     }
+
 
 @app.route("/auth", methods=["POST"])
 def auth():
+    input = request.json
+    print('input: ', input)
+    
+    username = input['username']
+    password = input['password']
+
+    selection = {
+        "username": username,
+        "password": password,
+    }
+    print('selection: ', selection)
+
+    projection = {"password": 0}
+
+    user = mdb.users.find_one(selection, projection)
+
+    if not (user and "_id" in user):
+        return {
+            "status": 0,
+            "cls": "danger",
+            "msg": "Invalid Credentials!..",
+            "payload": {},
+        }
+
     return {
         "status": 1,
         "cls": "success",
-        "msg": "Login Successful!.",
-        "payload": {},
+        "msg": "Login Successful!..",
+        "payload": {
+            "user": user,
+        },
     }
