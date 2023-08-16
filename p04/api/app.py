c5352357cb3487f95a05af2184f3fe6834168c68
Sivaraj S <contact@sdev.in>
2023-08-16 11:16:23 +0530

    Added dev.http and added new route to handle the todo list

--- p04/api/app.py
+++ p04/api/app.py
@@ -11,3 +11,30 @@ def home():
         "msg": "Home Route Success",
         "payload": {},
     }
+
+
+@app.route("/todo/list")
+def todoList():
+    items = [
+        {
+            "id": 1,
+            "title": "Task 1",
+        },
+        {
+            "id": 2,
+            "title": "Task 2",
+        },
+        {
+            "id": 3,
+            "title": "Task 3",
+        },
+    ]
+
+    return {
+        "status": 1,
+        "cls": "success",
+        "msg": "TODO List",
+        "payload": {
+            "items": items,
+        },
+    }
